6f553180127f396bc697a41cc7d3e662111510f9
Виталий Шлепаков <shlepakov58@gmail.com>
2020-07-16 12:47:00 +0200

    finish webpack configaration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,92 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const fileName = ext => !isProd
+    ? `bundle.${ext}`
+    : `bundle.[hash].${ext}`;
+
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    ]
+    if(!isProd){
+        loaders.push('eslint-loader')
+    }
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './index.js',
+    entry: ['@babel/polyfill', './index.js'],
     output: {
-        filename: 'bundle.js',
+        filename: fileName('js'),
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'scr/core')
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPugin({
+            template: "index.html",
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: fileName('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                          hmr: !isProd,
+                          reloadAll: true
+                      }
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                use: jsLoaders(),
+                exclude: /node_modules/,
+
+            }
+        ],
+    },
+    devtool: isProd ? false : 'source-map',
+    devServer: {
+        port: 3000,
+        hot: !isProd
     }
 };
